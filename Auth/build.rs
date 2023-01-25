2566c0ebd36abaf1ba852cf5ae4cad6cd4013a6a
Thallium Megalovania <tlmegalovania@qq.com>
2023-01-25 23:58:41 +0800

    submodule: update
    Signed-off-by: Thallium Megalovania <tlmegalovania@qq.com>

--- Auth/build.rs
+++ Auth/build.rs
@@ -1,7 +1,4 @@
 fn main() -> std::io::Result<()> {
     let builder = tonic_build::configure();
-    // if cfg!(not(test)) {
-    //     builder = builder.build_client(false);
-    // }
     builder.compile(&["proto/auth.proto"], &["proto"])
 }
